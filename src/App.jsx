9a87aeaf3296e0a0ab5df22daf1fdc4c821d63e1
Clarice Presley <claricepresley524@gmail.com>
2025-11-10 20:29:13 -0300

    adiciona componente da seção ao App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { StoriesHope } from "./components/StoriesHope";
+import BePart from "./components/BePart";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <StoriesHope />
+      <BePart />
     </BrowserRouter>
   );
 }
